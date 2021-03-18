5e80687bdf26f12f3f40fa5979b1eee2b74c4ac2
ggrubich <g.grubich.898@studms.ug.edu.pl>
2021-03-18 10:42:35 +0100

    Initial commit

---
+++ test/interpolation/MainTest.java
@@ -0,0 +1,11 @@
+package interpolation;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void testOk() {
+        assertTrue(true);
+    }
+}
